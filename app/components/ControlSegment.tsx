84e16bc9cb92e5d335775f9db491141e911f2db7
1718wani <ikuya1293@gmail.com>
2024-02-25 11:36:14 +0900

    💄 タブとして追加

--- app/components/ControlSegment.tsx
+++ app/components/ControlSegment.tsx
@@ -6,7 +6,7 @@ export const ControlSegment = () => {
       m={"sm"}
       radius="sm"
       size="sm"
-      data={["番組一覧", "お気に入り切り抜き", "新しい切り抜き"]}
+      data={["All", "Favorite", "New"]}
     />
   );
 };
