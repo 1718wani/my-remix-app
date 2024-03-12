d779fecb2fd5ebc41db2999984e146d8bf70bd1c
1718wani <ikuya1293@gmail.com>
2024-03-12 21:37:04 +0900

    💄 選択のUIを変更

--- app/routes/highlights.tsx
+++ app/routes/highlights.tsx
@@ -1,8 +1,21 @@
-import { ControlSegment } from "~/components/ControlSegment";
+import { Flex, Select, Title, rem } from "@mantine/core";
+import { ShareButton } from "~/features/Highlight/components/ShareButton";
 
 export default function Highlights() {
-    return (
-        <ControlSegment />
-    )
-
+  return (
+    <div>
+      <Flex justify={"space-between"} m={"md"}>
+        <Title order={2}>ハイライト一覧</Title>
+        <Select
+          withCheckIcon={false}
+          w={rem(120)}
+          defaultValue={"人気順"}
+          data={["人気順", "新しい順", "保存済み"]}
+          clearable={false}
+          allowDeselect={false}
+        />
+      </Flex>
+      <ShareButton />
+    </div>
+  );
 }
