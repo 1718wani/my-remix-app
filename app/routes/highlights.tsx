c6e248ec10f48ea657bb3e5de2e37d206a01f58c
1718wani <ikuya1293@gmail.com>
2024-03-15 10:27:43 +0900

    Selectを追加

--- app/routes/highlights.tsx
+++ app/routes/highlights.tsx
@@ -1,7 +1,35 @@
 import { Flex, Select, Title, rem } from "@mantine/core";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Outlet, useLoaderData, useNavigate } from "@remix-run/react";
+import { authenticator } from "~/features/Auth/services/authenticator";
 import { ShareButton } from "~/features/Highlight/components/ShareButton";
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const userId = await authenticator.isAuthenticated(request, {});
+
+  return json({ userId });
+};
+
 export default function Highlights() {
+  const data = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const handleChange = (value: string|null) => {
+    if (value === null) return;
+    switch (value) {
+      case "人気順":
+        navigate("/highlights/popular");
+        break;
+      case "新しい順":
+        navigate("/highlights/new");
+        break;
+      case "保存済み":
+        navigate("/highlights/saved");
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div>
       <Flex justify={"space-between"} m={"md"}>
@@ -13,9 +41,11 @@ export default function Highlights() {
           data={["人気順", "新しい順", "保存済み"]}
           clearable={false}
           allowDeselect={false}
+          onChange={handleChange}
         />
       </Flex>
-      <ShareButton />
+      <Outlet/>
+      <ShareButton userId={data.userId} />
     </div>
   );
 }
